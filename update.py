a79eca3854c9f6aadcad74a452de46636ba1048c
TableMagnet <human@tablemagnet.com>
2020-09-17 22:33:21 +0100

    Add autoremove option

--- update.py
+++ update.py
@@ -3,11 +3,53 @@ import time
 import sys
 
 
-username = sys.argv[1]
-password = sys.argv[2]
-
+flag = "UpdateVirtualBoxMachinesFinalSignalOperationIsComplete"
 binary = '"C:/Program Files/Oracle/VirtualBox/VBoxManage.exe"'
 newline = "\r\n"
+
+
+def parseArguments(argv):
+    if 3 > len(argv):
+        return False
+
+    arguments = {
+        "username": argv[1],
+        "password": argv[2],
+        "remove": False
+    }
+
+    argv = argv[3:]
+
+    while len(argv):
+        arg = argv.pop()
+
+        if "-r" == arg:
+            arguments["remove"] = True
+        else:
+            print("Unknown argument:", arg, newline)
+            return False
+
+    return arguments
+
+def printHelp():
+    print("""Usage:
+python update.py $username $password [arguments]
+
+Arguments
+-r  remove  Autoremove packages
+""")
+    exit()
+
+def getUpdateCommand(args):
+    command = ('echo {0} | sudo -S apt update && '
+        'echo {0} | sudo -S apt upgrade -y && '
+        '{1} echo {2}')
+
+    autoremove = ""
+    if args["remove"]:
+        autoremove = ("echo {0} | sudo -S apt autoremove -y &&").format(args["password"])
+
+    return command.format(args["password"], autoremove, flag)
 
 def vboxmanage(command):
     p = subprocess.Popen(binary + " " + command, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
@@ -37,20 +79,16 @@ def parseMachines(list):
         vm["uuid"] = line[line.index("\"") + 3:-1] # skip quote, space & braces
         vms.append(vm)
 
-def update(vm):
+def update(vm, args):
     vboxmanage("startvm {0}".format(vm["uuid"]))
     time.sleep(60) # Wait for VM to start
-    flag = "UpdateVirtualBoxMachinesFinalSignalOperationIsComplete"
     response = ""
 
     # Loop until VM is running and command succeeds
     while True:
         response = vboxmanage(('guestcontrol {0} --username {1} --password {2} '
-        'run --exe "/bin/sh" -- "/bin/sh" "-c" "'
-        'echo {2} | sudo -S apt update && '
-        'echo {2} | sudo -S apt upgrade -y && '
-        'echo {3}"'
-        ''.format(vm["uuid"], username, password, flag)))
+        'run --exe "/bin/sh" -- "/bin/sh" "-c" "{3}"'
+        ''.format(vm["uuid"], args["username"], args["password"], getUpdateCommand(args))))
 
         if "error:" in response:
             time.sleep(30)
@@ -68,7 +106,12 @@ def update(vm):
     return True
 
 def main():
+    args = parseArguments(sys.argv)
+    if not args:
+        printHelp()
+        return False
+
     machines = parseMachines(vboxmanage("list vms"))
-    update(machines[0])
+    update(machines[0], args)
 
 main()
